19c4ff3d0e5f3a858bebdaea930366fda94517fb
dfluechter <dominik.fluechter@googlemail.com>
2025-08-18 18:08:29 +0200

    mod allowed_hosts

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-secret")
 DEBUG = False
-ALLOWED_HOSTS = ["django-react-portfolio-s68r.onrender.com"]
+ALLOWED_HOSTS = ['django-react-portfolio-s68r.onrender.com', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
