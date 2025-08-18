9426c8fad996fa4d63dffbb1cb1d711162646491
dfluechter <dominik.fluechter@googlemail.com>
2025-08-18 20:59:41 +0200

    mod settings_prod

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -6,15 +6,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-secret")
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Get the hostname from the RENDER_EXTERNAL_HOSTNAME environment variable
 # if it exists.
 
-RENDER_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_HOSTNAME)
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS.extend(['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
